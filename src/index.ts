4e77919b46ea9a0f48e70683d38a193641e84741
Victor Kotseruba <barbuzaster@gmail.com>
2017-04-19 15:58:57 -0600

    export IField

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,4 @@ export { ArrayField } from "./ArrayField";
 export { Checkbox } from "./Checkbox";
 export { Input } from "./Input";
 export { FieldError } from "./FieldError";
+export { IField } from "./IField";
